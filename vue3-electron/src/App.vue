f60f6ebdad8e9160305f28c922dd59dacde3fc57
javilj <javilj@rpi.edu>
2024-11-22 16:46:58 -0500

    set up vue electron app

---
+++ vue3-electron/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="I hate white ppl"/> -->
+  <MainPage msg="white ppl"/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-electron/src/components/main.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1> I HATE {{ msg }}</h1>
+</template>
+
+<script>
+    export default {
+    name: 'MainPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
